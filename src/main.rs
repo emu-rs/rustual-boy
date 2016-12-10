84a7d9e1122475bffe114183a6caede7b4e44388
ferris <yupferris@gmail.com>
2016-12-10 15:07:13 +0100

    Add basic label support

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate nom;
 mod rom;
 mod interconnect;
 
-use nom::{IResult, eof, space, digit, hex_digit};
+use nom::{IResult, eof, space, digit, hex_digit, alphanumeric};
 
 use rom::*;
 use interconnect::*;
@@ -16,6 +16,7 @@ use std::io::{stdin, stdout, Write};
 use std::borrow::Cow;
 use std::str::{self, FromStr};
 use std::fmt;
+use std::collections::HashMap;
 
 #[derive(PartialEq, Eq)]
 enum Opcode {
@@ -76,11 +77,13 @@ impl InstructionFormat {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub enum Command {
     Goto(u32),
     ShowMem(Option<u32>),
     Disassemble(usize),
+    Label,
+    AddLabel(String, u32),
     Exit,
     Repeat,
 }
@@ -118,6 +121,7 @@ fn main() {
 
     let interconnect = Interconnect::new(rom);
 
+    let mut labels = HashMap::new();
     let mut cursor = 0xfffffff0;
     let mut last_command = None;
 
@@ -125,7 +129,7 @@ fn main() {
         print!("(vb-rs 0x{:08x}) > ", cursor);
         stdout().flush().unwrap();
 
-        let command = match (read_stdin().parse(), last_command) {
+        let command = match (read_stdin().parse(), last_command.clone()) {
             (Ok(Command::Repeat), Some(c)) => Ok(c),
             (Ok(Command::Repeat), None) => Err("No last command".into()),
             (Ok(c), _) => Ok(c),
@@ -140,6 +144,9 @@ fn main() {
                 if let Some(addr) = addr {
                     cursor = addr;
                 }
+
+                print_labels(&labels, cursor);
+
                 const NUM_ROWS: usize = 16;
                 const NUM_COLS: usize = 16;
                 for _ in 0..NUM_ROWS {
@@ -157,6 +164,8 @@ fn main() {
             }
             Ok(Command::Disassemble(count)) => {
                 for _ in 0..count {
+                    print_labels(&labels, cursor);
+
                     print!("0x{:08x}  ", cursor);
 
                     let first_halfword = interconnect.read_halfword(cursor);
@@ -207,6 +216,14 @@ fn main() {
                     }
                 }
             }
+            Ok(Command::Label) => {
+                for (name, addr) in labels.iter() {
+                    println!(".{}: 0x{:08x}", name, addr);
+                }
+            }
+            Ok(Command::AddLabel(ref name, addr)) => {
+                labels.insert(name.clone(), addr);
+            }
             Ok(Command::Exit) => break,
             Ok(Command::Repeat) => unreachable!(),
             Err(ref e) => println!("{}", e),
@@ -224,6 +241,12 @@ fn read_stdin() -> String {
     input.trim().into()
 }
 
+fn print_labels(labels: &HashMap<String, u32>, addr: u32) {
+    for (name, _) in labels.iter().filter(|x| *x.1 == addr) {
+        println!(".{}:", name);
+    }
+}
+
 named!(
     command<Command>,
     complete!(
@@ -232,6 +255,8 @@ named!(
             goto |
             show_mem |
             disassemble |
+            label |
+            add_label |
             exit |
             repeat),
         eof)));
@@ -274,9 +299,35 @@ named!(
     FromStr::from_str));
 
 named!(
+    label<Command>,
+    map!(
+        alt_complete!(tag!("label") | tag!("l")),
+    |_| Command::Label));
+
+named!(
+    add_label<Command>,
+    chain!(
+        alt_complete!(tag!("addlabel") | tag!("al")) ~
+        space ~
+        name: label_name ~
+        space ~
+        addr: hex_u32_parser,
+    || Command::AddLabel(name, addr)));
+
+named!(
+    label_name<String>,
+    preceded!(
+        char!('.'),
+        map_res!(
+            map_res!(
+                alphanumeric,
+                str::from_utf8),
+        FromStr::from_str)));
+
+named!(
     exit<Command>,
     map!(
-        alt_complete!(tag!("exit") | tag!("quit") | tag!("e") | tag!("q")),
+        alt_complete!(tag!("exit") | tag!("quit") | tag!("e") | tag!("x") | tag!("q")),
         |_| Command::Exit));
 
 named!(
