c141570bb04bc62d916468f5922220631139eedf
ferris <yupferris@gmail.com>
2017-01-08 23:55:19 +0100

    Remove top-level MappedAddress
    Similar to instruction encoding, this is a speed optimization and slight simplification. Adds a few subtractions to the rest of the code, but not too bad. Doesn't seem to have affected speed that much though, but perhaps the same pass on the VIP will.

--- src/interconnect.rs
+++ src/interconnect.rs
@@ -32,166 +32,174 @@ impl Interconnect {
     }
 
     pub fn read_byte(&mut self, addr: u32) -> u8 {
-        match map_address(addr) {
-            MappedAddress::Vip(addr) => self.vip.read_byte(addr),
-            MappedAddress::Vsu(addr) => self.vsu.read_byte(addr),
-            MappedAddress::LinkControlReg => {
+        let addr = addr & 0x07ffffff;
+        match addr {
+            VIP_START ... VIP_END => self.vip.read_byte(addr - VIP_START),
+            VSU_START ... VSU_END => self.vsu.read_byte(addr - VSU_START),
+            LINK_CONTROL_REG => {
                 logln!("WARNING: Read byte from Link Control Register not yet implemented");
                 0
             }
-            MappedAddress::AuxLinkReg => {
+            AUX_LINK_REG => {
                 logln!("WARNING: Read byte from Auxiliary Link Register not yet implemented");
                 0
             }
-            MappedAddress::LinkTransmitDataReg => {
+            LINK_TRANSMIT_DATA_REG => {
                 logln!("Read byte from Link Transmit Data Register not yet implemented");
                 0
             }
-            MappedAddress::LinkReceiveDataReg => {
+            LINK_RECEIVE_DATA_REG => {
                 logln!("Read byte from Link Receive Data Register not yet implemented");
                 0
             }
-            MappedAddress::GamePadInputLowReg => self.game_pad.read_input_low_reg(),
-            MappedAddress::GamePadInputHighReg => self.game_pad.read_input_high_reg(),
-            MappedAddress::TimerCounterReloadLowReg => self.timer.read_counter_reload_low_reg(),
-            MappedAddress::TimerCounterReloadHighReg => self.timer.read_counter_reload_high_reg(),
-            MappedAddress::TimerControlReg => self.timer.read_control_reg(),
-            MappedAddress::WaitControlReg => {
+            GAME_PAD_INPUT_LOW_REG => self.game_pad.read_input_low_reg(),
+            GAME_PAD_INPUT_HIGH_REG => self.game_pad.read_input_high_reg(),
+            TIMER_COUNTER_RELOAD_LOW_REG => self.timer.read_counter_reload_low_reg(),
+            TIMER_COUNTER_RELOAD_HIGH_REG => self.timer.read_counter_reload_high_reg(),
+            TIMER_CONTROL_REG => self.timer.read_control_reg(),
+            WAIT_CONTROL_REG => {
                 logln!("WARNING: Read byte from Wait Control Register not yet implemented");
                 0
             }
-            MappedAddress::GamePadInputControlReg => self.game_pad.read_input_control_reg(),
-            MappedAddress::CartridgeExpansion(addr) => {
-                logln!("WARNING: Read byte from Cartridge Expansion not yet implemented (addr: 0x{:08x})", addr);
+            GAME_PAD_INPUT_CONTROL_REG => self.game_pad.read_input_control_reg(),
+            CARTRIDGE_EXPANSION_START ... CARTRIDGE_EXPANSION_END => {
+                logln!("WARNING: Read byte from Cartridge Expansion not yet implemented (addr: 0x{:08x})", addr - CARTRIDGE_EXPANSION_START);
                 0
             }
-            MappedAddress::Wram(addr) => self.wram.read_byte(addr),
-            MappedAddress::CartridgeRam(addr) => self.sram.read_byte(addr),
-            MappedAddress::CartridgeRom(addr) => self.rom.read_byte(addr),
+            WRAM_START ... WRAM_END => self.wram.read_byte(addr - WRAM_START),
+            CARTRIDGE_RAM_START ... CARTRIDGE_RAM_END => self.sram.read_byte(addr - CARTRIDGE_RAM_START),
+            CARTRIDGE_ROM_START ... CARTRIDGE_ROM_END => self.rom.read_byte(addr - CARTRIDGE_ROM_START),
+            _ => panic!("Unrecognized addr: 0x{:08x}", addr)
         }
     }
 
     pub fn read_halfword(&mut self, addr: u32) -> u16 {
+        let addr = addr & 0x07ffffff;
         let addr = addr & 0xfffffffe;
-        match map_address(addr) {
-            MappedAddress::Vip(addr) => self.vip.read_halfword(addr),
-            MappedAddress::Vsu(addr) => self.vsu.read_halfword(addr),
-            MappedAddress::LinkControlReg => {
+        match addr {
+            VIP_START ... VIP_END => self.vip.read_halfword(addr - VIP_START),
+            VSU_START ... VSU_END => self.vsu.read_halfword(addr - VSU_START),
+            LINK_CONTROL_REG => {
                 logln!("Read halfword from Link Control Register not yet implemented");
                 0
             }
-            MappedAddress::AuxLinkReg => {
+            AUX_LINK_REG => {
                 logln!("Read halfword from Auxiliary Link Register not yet implemented");
                 0
             }
-            MappedAddress::LinkTransmitDataReg => {
+            LINK_TRANSMIT_DATA_REG => {
                 logln!("Read halfword from Link Transmit Data Register not yet implemented");
                 0
             }
-            MappedAddress::LinkReceiveDataReg => {
+            LINK_RECEIVE_DATA_REG => {
                 logln!("Read halfword from Link Receive Data Register not yet implemented");
                 0
             }
-            MappedAddress::GamePadInputLowReg => self.game_pad.read_input_low_reg() as _,
-            MappedAddress::GamePadInputHighReg => self.game_pad.read_input_high_reg() as _,
-            MappedAddress::TimerCounterReloadLowReg => self.timer.read_counter_reload_low_reg() as _,
-            MappedAddress::TimerCounterReloadHighReg => self.timer.read_counter_reload_high_reg() as _,
-            MappedAddress::TimerControlReg => self.timer.read_control_reg() as _,
-            MappedAddress::WaitControlReg => {
+            GAME_PAD_INPUT_LOW_REG => self.game_pad.read_input_low_reg() as _,
+            GAME_PAD_INPUT_HIGH_REG => self.game_pad.read_input_high_reg() as _,
+            TIMER_COUNTER_RELOAD_LOW_REG => self.timer.read_counter_reload_low_reg() as _,
+            TIMER_COUNTER_RELOAD_HIGH_REG => self.timer.read_counter_reload_high_reg() as _,
+            TIMER_CONTROL_REG => self.timer.read_control_reg() as _,
+            WAIT_CONTROL_REG => {
                 logln!("Read halfword from Wait Control Register not yet implemented");
                 0
             }
-            MappedAddress::GamePadInputControlReg => self.game_pad.read_input_control_reg() as _,
-            MappedAddress::CartridgeExpansion(addr) => {
-                logln!("WARNING: Read halfword from Cartridge Expansion not yet implemented (addr: 0x{:08x})", addr);
+            GAME_PAD_INPUT_CONTROL_REG => self.game_pad.read_input_control_reg() as _,
+            CARTRIDGE_EXPANSION_START ... CARTRIDGE_EXPANSION_END => {
+                logln!("WARNING: Read halfword from Cartridge Expansion not yet implemented (addr: 0x{:08x})", addr - CARTRIDGE_EXPANSION_START);
                 0
             }
-            MappedAddress::Wram(addr) => self.wram.read_halfword(addr),
-            MappedAddress::CartridgeRam(addr) => self.sram.read_halfword(addr),
-            MappedAddress::CartridgeRom(addr) => self.rom.read_halfword(addr),
+            WRAM_START ... WRAM_END => self.wram.read_halfword(addr - WRAM_START),
+            CARTRIDGE_RAM_START ... CARTRIDGE_RAM_END => self.sram.read_halfword(addr - CARTRIDGE_RAM_START),
+            CARTRIDGE_ROM_START ... CARTRIDGE_ROM_END => self.rom.read_halfword(addr - CARTRIDGE_ROM_START),
+            _ => panic!("Unrecognized addr: 0x{:08x}", addr)
         }
     }
 
     pub fn write_byte(&mut self, addr: u32, value: u8) {
-        match map_address(addr) {
-            MappedAddress::Vip(addr) => self.vip.write_byte(addr, value),
-            MappedAddress::Vsu(addr) => self.vsu.write_byte(addr, value),
-            MappedAddress::LinkControlReg => {
+        let addr = addr & 0x07ffffff;
+        match addr {
+            VIP_START ... VIP_END => self.vip.write_byte(addr - VIP_START, value),
+            VSU_START ... VSU_END => self.vsu.write_byte(addr - VSU_START, value),
+            LINK_CONTROL_REG => {
                 logln!("WARNING: Write byte to Link Control Register not yet implemented (value: 0x{:02x})", value);
             }
-            MappedAddress::AuxLinkReg => {
+            AUX_LINK_REG => {
                 logln!("WARNING: Write byte to Auxiliary Link Register not yet implemented (value: 0x{:02x})", value);
             }
-            MappedAddress::LinkTransmitDataReg => {
+            LINK_TRANSMIT_DATA_REG => {
                 logln!("WARNING: Write byte to Link Transmit Data Register not yet implemented (value: 0x{:02x})", value);
             }
-            MappedAddress::LinkReceiveDataReg => {
+            LINK_RECEIVE_DATA_REG => {
                 logln!("WARNING: Write byte to Link Receive Data Register not yet implemented (value: 0x{:02x})", value);
             }
-            MappedAddress::GamePadInputLowReg => {
+            GAME_PAD_INPUT_LOW_REG => {
                 logln!("WARNING: Attempted write byte to Game Pad Input Low Register (value: 0x{:02x})", value);
             }
-            MappedAddress::GamePadInputHighReg => {
+            GAME_PAD_INPUT_HIGH_REG => {
                 logln!("WARNING: Attempted write byte to Game Pad Input High Register (value: 0x{:02x})", value);
             }
-            MappedAddress::TimerCounterReloadLowReg => self.timer.write_counter_reload_low_reg(value),
-            MappedAddress::TimerCounterReloadHighReg => self.timer.write_counter_reload_high_reg(value),
-            MappedAddress::TimerControlReg => self.timer.write_control_reg(value),
-            MappedAddress::WaitControlReg => {
+            TIMER_COUNTER_RELOAD_LOW_REG => self.timer.write_counter_reload_low_reg(value),
+            TIMER_COUNTER_RELOAD_HIGH_REG => self.timer.write_counter_reload_high_reg(value),
+            TIMER_CONTROL_REG => self.timer.write_control_reg(value),
+            WAIT_CONTROL_REG => {
                 logln!("Wait Control Register (0x{:08x}) written: 0x{:02x}", addr, value);
                 logln!(" Cartridge ROM Waits: {}", if value & 0x01 == 0 { 2 } else { 1 });
                 logln!(" Cartridge Expansion Waits: {}", if value & 0x02 == 0 { 2 } else { 1 });
             }
-            MappedAddress::GamePadInputControlReg => self.game_pad.write_input_control_reg(value),
-            MappedAddress::CartridgeExpansion(addr) => {
-                logln!("WARNING: Write byte to Cartridge Expansion not yet implemented (addr: 0x{:08x}, value: 0x{:02x})", addr, value);
+            GAME_PAD_INPUT_CONTROL_REG => self.game_pad.write_input_control_reg(value),
+            CARTRIDGE_EXPANSION_START ... CARTRIDGE_EXPANSION_END => {
+                logln!("WARNING: Write byte to Cartridge Expansion not yet implemented (addr: 0x{:08x}, value: 0x{:02x})", addr - CARTRIDGE_EXPANSION_START, value);
             }
-            MappedAddress::Wram(addr) => self.wram.write_byte(addr, value),
-            MappedAddress::CartridgeRam(addr) => self.sram.write_byte(addr, value),
-            MappedAddress::CartridgeRom(_) => {
-                logln!("WARNING: Attempted write to Cartridge ROM at 0x{:08x}", addr);
+            WRAM_START ... WRAM_END => self.wram.write_byte(addr - WRAM_START, value),
+            CARTRIDGE_RAM_START ... CARTRIDGE_RAM_END => self.sram.write_byte(addr - CARTRIDGE_RAM_START, value),
+            CARTRIDGE_ROM_START ... CARTRIDGE_ROM_END => {
+                logln!("WARNING: Attempted write to Cartridge ROM at 0x{:08x}", addr - CARTRIDGE_ROM_START);
             }
+            _ => panic!("Unrecognized addr: 0x{:08x}", addr)
         }
     }
 
     pub fn write_halfword(&mut self, addr: u32, value: u16) {
+        let addr = addr & 0x07ffffff;
         let addr = addr & 0xfffffffe;
-        match map_address(addr) {
-            MappedAddress::Vip(addr) => self.vip.write_halfword(addr, value),
-            MappedAddress::Vsu(addr) => self.vsu.write_halfword(addr, value),
-            MappedAddress::LinkControlReg => {
+        match addr {
+            VIP_START ... VIP_END => self.vip.write_halfword(addr - VIP_START, value),
+            VSU_START ... VSU_END => self.vsu.write_halfword(addr - VSU_START, value),
+            LINK_CONTROL_REG => {
                 logln!("WARNING: Write halfword to Link Control Register not yet implemented (value: 0x{:04x})", value);
             }
-            MappedAddress::AuxLinkReg => {
+            AUX_LINK_REG => {
                 logln!("WARNING: Write halfword to Auxiliary Link Register not yet implemented (value: 0x{:04x})", value);
             }
-            MappedAddress::LinkTransmitDataReg => {
+            LINK_TRANSMIT_DATA_REG => {
                 logln!("WARNING: Write halfword to Link Transmit Data Register not yet implemented (value: 0x{:04x})", value);
             }
-            MappedAddress::LinkReceiveDataReg => {
+            LINK_RECEIVE_DATA_REG => {
                 logln!("WARNING: Write halfword to Link Receive Data Register not yet implemented (value: 0x{:04x})", value);
             }
-            MappedAddress::GamePadInputLowReg => {
+            GAME_PAD_INPUT_LOW_REG => {
                 logln!("WARNING: Attempted halfword byte to Game Pad Input Low Register (value: 0x{:04x})", value);
             }
-            MappedAddress::GamePadInputHighReg => {
+            GAME_PAD_INPUT_HIGH_REG => {
                 logln!("WARNING: Attempted halfword byte to Game Pad Input High Register (value: 0x{:04x})", value);
             }
-            MappedAddress::TimerCounterReloadLowReg => self.timer.write_counter_reload_low_reg(value as _),
-            MappedAddress::TimerCounterReloadHighReg => self.timer.write_counter_reload_high_reg(value as _),
-            MappedAddress::TimerControlReg => self.timer.write_control_reg(value as _),
-            MappedAddress::WaitControlReg => {
+            TIMER_COUNTER_RELOAD_LOW_REG => self.timer.write_counter_reload_low_reg(value as _),
+            TIMER_COUNTER_RELOAD_HIGH_REG => self.timer.write_counter_reload_high_reg(value as _),
+            TIMER_CONTROL_REG => self.timer.write_control_reg(value as _),
+            WAIT_CONTROL_REG => {
                 logln!("WARNING: Write halfword to Wait Control Register not yet implemented (value: 0x{:04x})", value);
             }
-            MappedAddress::GamePadInputControlReg => self.game_pad.write_input_control_reg(value as _),
-            MappedAddress::CartridgeExpansion(addr) => {
-                logln!("WARNING: Write halfword to Cartridge Expansion not yet implemented (addr: 0x{:08x}, value: 0x{:04x})", addr, value);
+            GAME_PAD_INPUT_CONTROL_REG => self.game_pad.write_input_control_reg(value as _),
+            CARTRIDGE_EXPANSION_START ... CARTRIDGE_EXPANSION_END => {
+                logln!("WARNING: Write halfword to Cartridge Expansion not yet implemented (addr: 0x{:08x}, value: 0x{:04x})", addr - CARTRIDGE_EXPANSION_START, value);
             }
-            MappedAddress::Wram(addr) => self.wram.write_halfword(addr, value),
-            MappedAddress::CartridgeRam(addr) => self.sram.write_halfword(addr, value),
-            MappedAddress::CartridgeRom(_) => {
-                logln!("WARNING: Attempted write to Cartridge ROM at 0x{:08x}", addr);
+            WRAM_START ... WRAM_END => self.wram.write_halfword(addr - WRAM_START, value),
+            CARTRIDGE_RAM_START ... CARTRIDGE_RAM_END => self.sram.write_halfword(addr - CARTRIDGE_RAM_START, value),
+            CARTRIDGE_ROM_START ... CARTRIDGE_ROM_END => {
+                logln!("WARNING: Attempted write to Cartridge ROM at 0x{:08x}", addr - CARTRIDGE_ROM_START);
             }
+            _ => panic!("Unrecognized addr: 0x{:08x}", addr)
         }
     }
 
