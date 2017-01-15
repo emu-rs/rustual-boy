6bbeb589a4734ba23f51c4d88cb9c2916b8d7685
ferris <yupferris@gmail.com>
2017-01-15 06:48:05 +0100

    Move voice code around a lot
    I'm up way too late and not thinking very clearly, just poking at code until an abstraction emerges while watching agdq. Onto something probably, but time for bed. :)

--- src/vsu/mod.rs
+++ src/vsu/mod.rs
@@ -20,8 +20,42 @@ const CPU_CYCLE_PERIOD_NS: u64 = 50;
 
 const FREQUENCY_CLOCK_PERIOD_NS: u64 = S_TO_NS / 5000000;
 
-#[derive(Default, Clone)]
-struct Voice {
+#[derive(Default)]
+struct Envelope {
+    reg_data_reload: usize,
+    reg_data_direction: bool,
+    reg_data_step_interval: usize,
+
+    reg_control_repeat: bool,
+    reg_control_enable: bool,
+}
+
+impl Envelope {
+    fn read_data_reg(&self) -> u8 {
+        ((self.reg_data_reload as u8) << 4) |
+        (if self.reg_data_direction { 1 } else { 0 } << 3) |
+        (self.reg_data_step_interval as u8)
+    }
+
+    fn write_data_reg(&mut self, value: u8) {
+        self.reg_data_reload = (value >> 4) as _;
+        self.reg_data_direction = (value & 0x80) != 0;
+        self.reg_data_step_interval = (value & 0x07) as _;
+    }
+
+    fn read_control_reg(&self) -> u8 {
+        (if self.reg_control_repeat { 1 } else { 0 } << 1) |
+        (if self.reg_control_enable { 1 } else { 0 })
+    }
+
+    fn write_control_reg(&mut self, value: u8) {
+        self.reg_control_repeat = (value & 0x02) != 0;
+        self.reg_control_enable = (value & 0x01) != 0;
+    }
+}
+
+#[derive(Default)]
+struct StandardVoice {
     reg_play_control_enable: bool,
     reg_play_control_use_duration: bool,
     reg_play_control_duration: usize,
@@ -33,12 +67,7 @@ struct Voice {
 
     reg_frequency_high: usize,
 
-    reg_envelope_data_reload: usize,
-    reg_envelope_data_direction: bool,
-    reg_envelope_data_step_interval: usize,
-
-    reg_envelope_control_repeat: bool,
-    reg_envelope_control_enable: bool,
+    envelope: Envelope,
 
     reg_pcm_wave: usize,
 
@@ -47,7 +76,7 @@ struct Voice {
     phase: usize,
 }
 
-impl Voice {
+impl StandardVoice {
     fn read_play_control_reg(&self) -> u8 {
         (if self.reg_play_control_enable { 1 } else { 0 } << 7) |
         (if self.reg_play_control_use_duration { 1 } else { 0 } << 5) |
@@ -62,6 +91,7 @@ impl Voice {
         if self.reg_play_control_enable {
             self.frequency_clock_counter = 0;
             self.frequency_counter = 0;
+            self.phase = 0;
         }
     }
 
@@ -92,25 +122,19 @@ impl Voice {
     }
 
     fn read_envelope_data_reg(&self) -> u8 {
-        ((self.reg_envelope_data_reload as u8) << 4) |
-        (if self.reg_envelope_data_direction { 1 } else { 0 } << 3) |
-        (self.reg_envelope_data_step_interval as u8)
+        self.envelope.read_data_reg()
     }
 
     fn write_envelope_data_reg(&mut self, value: u8) {
-        self.reg_envelope_data_reload = (value >> 4) as _;
-        self.reg_envelope_data_direction = (value & 0x80) != 0;
-        self.reg_envelope_data_step_interval = (value & 0x07) as _;
+        self.envelope.write_data_reg(value);
     }
 
     fn read_envelope_control_reg(&self) -> u8 {
-        (if self.reg_envelope_control_repeat { 1 } else { 0 } << 1) |
-        (if self.reg_envelope_control_enable { 1 } else { 0 })
+        self.envelope.read_control_reg()
     }
 
     fn write_envelope_control_reg(&mut self, value: u8) {
-        self.reg_envelope_control_repeat = (value & 0x02) != 0;
-        self.reg_envelope_control_enable = (value & 0x01) != 0;
+        self.envelope.write_control_reg(value);
     }
 
     fn read_pcm_wave_reg(&self) -> u8 {
@@ -129,14 +153,25 @@ impl Voice {
             self.frequency_counter += 1;
             if self.frequency_counter >= 2048 - ((self.reg_frequency_high << 8) | self.reg_frequency_low) {
                 self.frequency_counter = 0;
-                self.phase = (self.phase + 1) & (NUM_WAVE_TABLE_WORDS - 1);
+
+                self.frequency_clock();
             }
         }
     }
 
+    fn frequency_clock(&mut self) {
+        self.phase = (self.phase + 1) & (NUM_WAVE_TABLE_WORDS - 1);
+    }
+
     fn sample(&self, wave_tables: &[u8]) -> (usize, usize) {
+        let wave_level = wave_tables[self.reg_pcm_wave * NUM_WAVE_TABLE_WORDS + self.phase] as usize;
+
+        self.mix_sample(wave_level)
+    }
+
+    fn mix_sample(&self, sound_data: usize) -> (usize, usize) {
         if self.reg_play_control_enable {
-            let envelope_level = 0x0f;//if self.reg_envelope_control_enable { 0x08 } else { 0x00 };
+            let envelope_level = 0x0f;//if self.envelope.reg_control_enable { 0x08 } else { 0x00 };
 
             let left_level = if self.reg_volume_left == 0 || envelope_level == 0 {
                 0
@@ -149,10 +184,8 @@ impl Voice {
                 (self.reg_volume_right * envelope_level) + 1
             };
 
-            let wave_level = wave_tables[self.reg_pcm_wave * NUM_WAVE_TABLE_WORDS + self.phase] as usize;
-
-            let output_left = (wave_level * left_level) >> 1;
-            let output_right = (wave_level * right_level) >> 1;
+            let output_left = (sound_data * left_level) >> 1;
+            let output_right = (sound_data * right_level) >> 1;
 
             (output_left, output_right)
         } else {
@@ -164,7 +197,11 @@ impl Voice {
 pub struct Vsu {
     wave_tables: Box<[u8]>,
 
-    voices: Box<[Voice]>,
+    voice1: StandardVoice,
+    voice2: StandardVoice,
+    voice3: StandardVoice,
+    voice4: StandardVoice,
+    voice5: StandardVoice,
 
     reg_sound_disable: bool,
 
@@ -176,7 +213,11 @@ impl Vsu {
         Vsu {
             wave_tables: vec![0; TOTAL_WAVE_TABLE_SIZE].into_boxed_slice(),
 
-            voices: vec![Voice::default(); 5].into_boxed_slice(),
+            voice1: StandardVoice::default(),
+            voice2: StandardVoice::default(),
+            voice3: StandardVoice::default(),
+            voice4: StandardVoice::default(),
+            voice5: StandardVoice::default(),
 
             reg_sound_disable: false,
 
@@ -191,41 +232,41 @@ impl Vsu {
             PCM_WAVE_TABLE_2_START ... PCM_WAVE_TABLE_2_END => self.wave_tables[((addr - PCM_WAVE_TABLE_2_START) / 4 + 0x40) as usize],
             PCM_WAVE_TABLE_3_START ... PCM_WAVE_TABLE_3_END => self.wave_tables[((addr - PCM_WAVE_TABLE_3_START) / 4 + 0x60) as usize],
             PCM_WAVE_TABLE_4_START ... PCM_WAVE_TABLE_4_END => self.wave_tables[((addr - PCM_WAVE_TABLE_4_START) / 4 + 0x80) as usize],
-            VOICE_1_PLAY_CONTROL => self.voices[0].read_play_control_reg(),
-            VOICE_1_VOLUME => self.voices[0].read_volume_reg(),
-            VOICE_1_FREQUENCY_LOW => self.voices[0].read_frequency_low_reg(),
-            VOICE_1_FREQUENCY_HIGH => self.voices[0].read_frequency_high_reg(),
-            VOICE_1_ENVELOPE_DATA => self.voices[0].read_envelope_data_reg(),
-            VOICE_1_ENVELOPE_CONTROL => self.voices[0].read_envelope_control_reg(),
-            VOICE_1_PCM_WAVE => self.voices[0].read_pcm_wave_reg(),
-            VOICE_2_PLAY_CONTROL => self.voices[1].read_play_control_reg(),
-            VOICE_2_VOLUME => self.voices[1].read_volume_reg(),
-            VOICE_2_FREQUENCY_LOW => self.voices[1].read_frequency_low_reg(),
-            VOICE_2_FREQUENCY_HIGH => self.voices[1].read_frequency_high_reg(),
-            VOICE_2_ENVELOPE_DATA => self.voices[1].read_envelope_data_reg(),
-            VOICE_2_ENVELOPE_CONTROL => self.voices[1].read_envelope_control_reg(),
-            VOICE_2_PCM_WAVE => self.voices[1].read_pcm_wave_reg(),
-            VOICE_3_PLAY_CONTROL => self.voices[2].read_play_control_reg(),
-            VOICE_3_VOLUME => self.voices[2].read_volume_reg(),
-            VOICE_3_FREQUENCY_LOW => self.voices[2].read_frequency_low_reg(),
-            VOICE_3_FREQUENCY_HIGH => self.voices[2].read_frequency_high_reg(),
-            VOICE_3_ENVELOPE_DATA => self.voices[2].read_envelope_data_reg(),
-            VOICE_3_ENVELOPE_CONTROL => self.voices[2].read_envelope_control_reg(),
-            VOICE_3_PCM_WAVE => self.voices[2].read_pcm_wave_reg(),
-            VOICE_4_PLAY_CONTROL => self.voices[3].read_play_control_reg(),
-            VOICE_4_VOLUME => self.voices[3].read_volume_reg(),
-            VOICE_4_FREQUENCY_LOW => self.voices[3].read_frequency_low_reg(),
-            VOICE_4_FREQUENCY_HIGH => self.voices[3].read_frequency_high_reg(),
-            VOICE_4_ENVELOPE_DATA => self.voices[3].read_envelope_data_reg(),
-            VOICE_4_ENVELOPE_CONTROL => self.voices[3].read_envelope_control_reg(),
-            VOICE_4_PCM_WAVE => self.voices[3].read_pcm_wave_reg(),
-            VOICE_5_PLAY_CONTROL => self.voices[4].read_play_control_reg(),
-            VOICE_5_VOLUME => self.voices[4].read_volume_reg(),
-            VOICE_5_FREQUENCY_LOW => self.voices[4].read_frequency_low_reg(),
-            VOICE_5_FREQUENCY_HIGH => self.voices[4].read_frequency_high_reg(),
-            VOICE_5_ENVELOPE_DATA => self.voices[4].read_envelope_data_reg(),
-            VOICE_5_ENVELOPE_CONTROL => self.voices[4].read_envelope_control_reg(),
-            VOICE_5_PCM_WAVE => self.voices[4].read_pcm_wave_reg(),
+            VOICE_1_PLAY_CONTROL => self.voice1.read_play_control_reg(),
+            VOICE_1_VOLUME => self.voice1.read_volume_reg(),
+            VOICE_1_FREQUENCY_LOW => self.voice1.read_frequency_low_reg(),
+            VOICE_1_FREQUENCY_HIGH => self.voice1.read_frequency_high_reg(),
+            VOICE_1_ENVELOPE_DATA => self.voice1.read_envelope_data_reg(),
+            VOICE_1_ENVELOPE_CONTROL => self.voice1.read_envelope_control_reg(),
+            VOICE_1_PCM_WAVE => self.voice1.read_pcm_wave_reg(),
+            VOICE_2_PLAY_CONTROL => self.voice2.read_play_control_reg(),
+            VOICE_2_VOLUME => self.voice2.read_volume_reg(),
+            VOICE_2_FREQUENCY_LOW => self.voice2.read_frequency_low_reg(),
+            VOICE_2_FREQUENCY_HIGH => self.voice2.read_frequency_high_reg(),
+            VOICE_2_ENVELOPE_DATA => self.voice2.read_envelope_data_reg(),
+            VOICE_2_ENVELOPE_CONTROL => self.voice2.read_envelope_control_reg(),
+            VOICE_2_PCM_WAVE => self.voice2.read_pcm_wave_reg(),
+            VOICE_3_PLAY_CONTROL => self.voice3.read_play_control_reg(),
+            VOICE_3_VOLUME => self.voice3.read_volume_reg(),
+            VOICE_3_FREQUENCY_LOW => self.voice3.read_frequency_low_reg(),
+            VOICE_3_FREQUENCY_HIGH => self.voice3.read_frequency_high_reg(),
+            VOICE_3_ENVELOPE_DATA => self.voice3.read_envelope_data_reg(),
+            VOICE_3_ENVELOPE_CONTROL => self.voice3.read_envelope_control_reg(),
+            VOICE_3_PCM_WAVE => self.voice3.read_pcm_wave_reg(),
+            VOICE_4_PLAY_CONTROL => self.voice4.read_play_control_reg(),
+            VOICE_4_VOLUME => self.voice4.read_volume_reg(),
+            VOICE_4_FREQUENCY_LOW => self.voice4.read_frequency_low_reg(),
+            VOICE_4_FREQUENCY_HIGH => self.voice4.read_frequency_high_reg(),
+            VOICE_4_ENVELOPE_DATA => self.voice4.read_envelope_data_reg(),
+            VOICE_4_ENVELOPE_CONTROL => self.voice4.read_envelope_control_reg(),
+            VOICE_4_PCM_WAVE => self.voice4.read_pcm_wave_reg(),
+            VOICE_5_PLAY_CONTROL => self.voice5.read_play_control_reg(),
+            VOICE_5_VOLUME => self.voice5.read_volume_reg(),
+            VOICE_5_FREQUENCY_LOW => self.voice5.read_frequency_low_reg(),
+            VOICE_5_FREQUENCY_HIGH => self.voice5.read_frequency_high_reg(),
+            VOICE_5_ENVELOPE_DATA => self.voice5.read_envelope_data_reg(),
+            VOICE_5_ENVELOPE_CONTROL => self.voice5.read_envelope_control_reg(),
+            VOICE_5_PCM_WAVE => self.voice5.read_pcm_wave_reg(),
             SOUND_DISABLE_REG => if self.reg_sound_disable { 1 } else { 0 },
             _ => {
                 logln!("VSU read byte not yet implemented (addr: 0x{:08x})", addr);
@@ -241,41 +282,41 @@ impl Vsu {
             PCM_WAVE_TABLE_2_START ... PCM_WAVE_TABLE_2_END => self.wave_tables[((addr - PCM_WAVE_TABLE_2_START) / 4 + 0x40) as usize] = value & 0x3f,
             PCM_WAVE_TABLE_3_START ... PCM_WAVE_TABLE_3_END => self.wave_tables[((addr - PCM_WAVE_TABLE_3_START) / 4 + 0x60) as usize] = value & 0x3f,
             PCM_WAVE_TABLE_4_START ... PCM_WAVE_TABLE_4_END => self.wave_tables[((addr - PCM_WAVE_TABLE_4_START) / 4 + 0x80) as usize] = value & 0x3f,
-            VOICE_1_PLAY_CONTROL => self.voices[0].write_play_control_reg(value),
-            VOICE_1_VOLUME => self.voices[0].write_volume_reg(value),
-            VOICE_1_FREQUENCY_LOW => self.voices[0].write_frequency_low_reg(value),
-            VOICE_1_FREQUENCY_HIGH => self.voices[0].write_frequency_high_reg(value),
-            VOICE_1_ENVELOPE_DATA => self.voices[0].write_envelope_data_reg(value),
-            VOICE_1_ENVELOPE_CONTROL => self.voices[0].write_envelope_control_reg(value),
-            VOICE_1_PCM_WAVE => self.voices[0].write_pcm_wave_reg(value),
-            VOICE_2_PLAY_CONTROL => self.voices[1].write_play_control_reg(value),
-            VOICE_2_VOLUME => self.voices[1].write_volume_reg(value),
-            VOICE_2_FREQUENCY_LOW => self.voices[1].write_frequency_low_reg(value),
-            VOICE_2_FREQUENCY_HIGH => self.voices[1].write_frequency_high_reg(value),
-            VOICE_2_ENVELOPE_DATA => self.voices[1].write_envelope_data_reg(value),
-            VOICE_2_ENVELOPE_CONTROL => self.voices[1].write_envelope_control_reg(value),
-            VOICE_2_PCM_WAVE => self.voices[1].write_pcm_wave_reg(value),
-            VOICE_3_PLAY_CONTROL => self.voices[2].write_play_control_reg(value),
-            VOICE_3_VOLUME => self.voices[2].write_volume_reg(value),
-            VOICE_3_FREQUENCY_LOW => self.voices[2].write_frequency_low_reg(value),
-            VOICE_3_FREQUENCY_HIGH => self.voices[2].write_frequency_high_reg(value),
-            VOICE_3_ENVELOPE_DATA => self.voices[2].write_envelope_data_reg(value),
-            VOICE_3_ENVELOPE_CONTROL => self.voices[2].write_envelope_control_reg(value),
-            VOICE_3_PCM_WAVE => self.voices[2].write_pcm_wave_reg(value),
-            VOICE_4_PLAY_CONTROL => self.voices[3].write_play_control_reg(value),
-            VOICE_4_VOLUME => self.voices[3].write_volume_reg(value),
-            VOICE_4_FREQUENCY_LOW => self.voices[3].write_frequency_low_reg(value),
-            VOICE_4_FREQUENCY_HIGH => self.voices[3].write_frequency_high_reg(value),
-            VOICE_4_ENVELOPE_DATA => self.voices[3].write_envelope_data_reg(value),
-            VOICE_4_ENVELOPE_CONTROL => self.voices[3].write_envelope_control_reg(value),
-            VOICE_4_PCM_WAVE => self.voices[3].write_pcm_wave_reg(value),
-            VOICE_5_PLAY_CONTROL => self.voices[4].write_play_control_reg(value),
-            VOICE_5_VOLUME => self.voices[4].write_volume_reg(value),
-            VOICE_5_FREQUENCY_LOW => self.voices[4].write_frequency_low_reg(value),
-            VOICE_5_FREQUENCY_HIGH => self.voices[4].write_frequency_high_reg(value),
-            VOICE_5_ENVELOPE_DATA => self.voices[4].write_envelope_data_reg(value),
-            VOICE_5_ENVELOPE_CONTROL => self.voices[4].write_envelope_control_reg(value),
-            VOICE_5_PCM_WAVE => self.voices[4].write_pcm_wave_reg(value),
+            VOICE_1_PLAY_CONTROL => self.voice1.write_play_control_reg(value),
+            VOICE_1_VOLUME => self.voice1.write_volume_reg(value),
+            VOICE_1_FREQUENCY_LOW => self.voice1.write_frequency_low_reg(value),
+            VOICE_1_FREQUENCY_HIGH => self.voice1.write_frequency_high_reg(value),
+            VOICE_1_ENVELOPE_DATA => self.voice1.write_envelope_data_reg(value),
+            VOICE_1_ENVELOPE_CONTROL => self.voice1.write_envelope_control_reg(value),
+            VOICE_1_PCM_WAVE => self.voice1.write_pcm_wave_reg(value),
+            VOICE_2_PLAY_CONTROL => self.voice2.write_play_control_reg(value),
+            VOICE_2_VOLUME => self.voice2.write_volume_reg(value),
+            VOICE_2_FREQUENCY_LOW => self.voice2.write_frequency_low_reg(value),
+            VOICE_2_FREQUENCY_HIGH => self.voice2.write_frequency_high_reg(value),
+            VOICE_2_ENVELOPE_DATA => self.voice2.write_envelope_data_reg(value),
+            VOICE_2_ENVELOPE_CONTROL => self.voice2.write_envelope_control_reg(value),
+            VOICE_2_PCM_WAVE => self.voice2.write_pcm_wave_reg(value),
+            VOICE_3_PLAY_CONTROL => self.voice3.write_play_control_reg(value),
+            VOICE_3_VOLUME => self.voice3.write_volume_reg(value),
+            VOICE_3_FREQUENCY_LOW => self.voice3.write_frequency_low_reg(value),
+            VOICE_3_FREQUENCY_HIGH => self.voice3.write_frequency_high_reg(value),
+            VOICE_3_ENVELOPE_DATA => self.voice3.write_envelope_data_reg(value),
+            VOICE_3_ENVELOPE_CONTROL => self.voice3.write_envelope_control_reg(value),
+            VOICE_3_PCM_WAVE => self.voice3.write_pcm_wave_reg(value),
+            VOICE_4_PLAY_CONTROL => self.voice4.write_play_control_reg(value),
+            VOICE_4_VOLUME => self.voice4.write_volume_reg(value),
+            VOICE_4_FREQUENCY_LOW => self.voice4.write_frequency_low_reg(value),
+            VOICE_4_FREQUENCY_HIGH => self.voice4.write_frequency_high_reg(value),
+            VOICE_4_ENVELOPE_DATA => self.voice4.write_envelope_data_reg(value),
+            VOICE_4_ENVELOPE_CONTROL => self.voice4.write_envelope_control_reg(value),
+            VOICE_4_PCM_WAVE => self.voice4.write_pcm_wave_reg(value),
+            VOICE_5_PLAY_CONTROL => self.voice5.write_play_control_reg(value),
+            VOICE_5_VOLUME => self.voice5.write_volume_reg(value),
+            VOICE_5_FREQUENCY_LOW => self.voice5.write_frequency_low_reg(value),
+            VOICE_5_FREQUENCY_HIGH => self.voice5.write_frequency_high_reg(value),
+            VOICE_5_ENVELOPE_DATA => self.voice5.write_envelope_data_reg(value),
+            VOICE_5_ENVELOPE_CONTROL => self.voice5.write_envelope_control_reg(value),
+            VOICE_5_PCM_WAVE => self.voice5.write_pcm_wave_reg(value),
             SOUND_DISABLE_REG => {
                 // This might actually be a strobe register
                 self.reg_sound_disable = (value & 0x01) != 0;
@@ -296,9 +337,11 @@ impl Vsu {
 
     pub fn cycles(&mut self, cycles: usize, audio_driver: &mut AudioDriver) {
         for _ in 0..cycles {
-            for voice in self.voices.iter_mut() {
-                voice.cycle();
-            }
+            self.voice1.cycle();
+            self.voice2.cycle();
+            self.voice3.cycle();
+            self.voice4.cycle();
+            self.voice5.cycle();
 
             self.sample_clock_counter += CPU_CYCLE_PERIOD_NS;
             if self.sample_clock_counter >= SAMPLE_PERIOD_NS {
@@ -309,11 +352,23 @@ impl Vsu {
                 } else {*/
                     let mut voice_acc_left = 0;
                     let mut voice_acc_right = 0;
-                    for voice in self.voices.iter() {
-                        let (left, right) = voice.sample(&self.wave_tables);
-                        voice_acc_left += left;
-                        voice_acc_right += right;
-                    }
+
+                    let (voice_left, voice_right) = self.voice1.sample(&self.wave_tables);
+                    voice_acc_left += voice_left;
+                    voice_acc_right += voice_right;
+                    let (voice_left, voice_right) = self.voice2.sample(&self.wave_tables);
+                    voice_acc_left += voice_left;
+                    voice_acc_right += voice_right;
+                    let (voice_left, voice_right) = self.voice3.sample(&self.wave_tables);
+                    voice_acc_left += voice_left;
+                    voice_acc_right += voice_right;
+                    let (voice_left, voice_right) = self.voice4.sample(&self.wave_tables);
+                    voice_acc_left += voice_left;
+                    voice_acc_right += voice_right;
+                    let (voice_left, voice_right) = self.voice5.sample(&self.wave_tables);
+                    voice_acc_left += voice_left;
+                    voice_acc_right += voice_right;
+
                     let output_left = (voice_acc_left & 0xfff8) as i16;
                     let output_right = (voice_acc_right & 0xfff8) as i16;
 
